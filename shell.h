535bb74d677da8cf36a2584c29eec651dfceb84c
epaschke22 <ericksonpaschke@gmail.com>
2020-11-13 17:23:21 +0000

    reformating string to double pointer function as own function and including print environment function

--- shell.h
+++ shell.h
@@ -14,4 +14,5 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 void free_double(char **dptr);
+char **str_to_double(char *input, char *delm);
 #endif
