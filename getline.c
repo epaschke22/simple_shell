d79808b7ab1b1028c462a44c2eb6cf0ae9578481
Your Name <you@example.com>
2020-11-14 00:45:37 -0500

    not working transferring files to another pc

--- getline.c
+++ getline.c
@@ -4,7 +4,7 @@ int main()
 {
 	char *buffer = NULL, **input, **path, *adress;
 	size_t bufsize = 0, inputsize = 0;
-	int i, fd;
+	int i, fd, inputCounter = 0;
 
 	path = str_to_double(getenv("PATH"), ":");
 	while (1)
@@ -18,7 +18,39 @@ int main()
 			fd = access(adress, F_OK);
 			if (fd == 0)
 			{
-				printf("The file exists!\n");
+
+				
+				printf("The file exists!, path: %s\n", adress);
+/*
+				for (inputCounter = 0; input[inputCounter] != NULL; inputCounter++);
+				printf("input Counter: %d, word: %s\n", inputCounter, input[inputCounter - 1]);
+				char *argv[inputCounter + 1];
+
+				inputCounter = 0;
+
+				while (input[inputCounter] != NULL)
+				{
+					argv[inputCounter] = input[inputCounter];
+					inputCounter++;
+				}
+				argv[inputCounter + 1] = NULL;
+
+				char *argv2[] = {"ls", "-a",  NULL};
+				inputCounter = 0;
+				while (input[inputCounter] != NULL)
+				{
+					printf("count: %d, argv: %s\n", inputCounter, input[inputCounter]);
+					inputCounter++;
+				}
+				inputCounter = 0;
+				while (argv2[inputCounter] != NULL)
+				{
+					printf("count: %d, argv2: %s\n", inputCounter, argv2[inputCounter]);
+					inputCounter++;
+				}
+*/
+				if (execve(adress, input, NULL) == -1)
+					printf("Execve Error\n");
 				break;
 			}
 		}
