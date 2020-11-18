be1e8f09cfc273fbf586b7bc64bc104473ae5356
epaschke22 <ericksonpaschke@gmail.com>
2020-11-18 20:48:13 +0000

    updating print envionment just in case

--- printenv.c
+++ printenv.c
@@ -1,13 +1,22 @@
 #include <unistd.h>
 #include "shell.h"
+
 int main(int argc, char **argv, char **env)
 {
 	int i;
 	char **envpath;
+	char *test, *cat;
 
-	envpath = str_to_double(getenv("PATH"), ":");
+	test = _getenv("PATH", env);
+	printf("%s\n", test);
+	envpath = str_to_double(test, ":");
+	free(test);
 	for (i = 0; envpath[i]; i++)
-		printf("%s\n", envpath[i]);
+	{
+		cat = catcmd(envpath[i], "ls");
+		printf("%s\n", cat);
+		free(cat);
+	}
 	free_double(envpath);
 	return (0);
 }
