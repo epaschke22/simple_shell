d9d86edf521c79a919a0e23aa7ef267a84ee46a5
epaschke22 <ericksonpaschke@gmail.com>
2020-11-18 18:11:53 +0000

    updating string to double function to fix memory issues

--- str_to_double.c
+++ str_to_double.c
@@ -9,12 +9,12 @@
 char **str_to_double(char *input, char *delm)
 {
 	char **output, *item;
-	unsigned int cmdlen = 0, i;
+	unsigned int cmdlen = 0, i = 0;
 
 	for (i = 0; input[i]; i++)
 		if (input[i] == delm[0])
 			cmdlen++;
-	output = malloc(sizeof(char *) * (cmdlen + 1));
+	output = malloc(sizeof(char *) * (cmdlen + 2));
 	if (output == NULL)
 		return(NULL);
 	item = strtok(input, delm);
